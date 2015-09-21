68926955f9d3a720f136ed45e0ebe5cf883aeb55
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-09-21 14:40:12 +0200

    make SoftRefVoaltileCacheImp a singleton

--- src/main/java/bdv/img/cache/VolatileGlobalCellCache.java
+++ src/main/java/bdv/img/cache/VolatileGlobalCellCache.java
@@ -105,7 +105,7 @@ public class VolatileGlobalCellCache implements Cache
 		}
 	}
 
-	protected VolatileCache volatileCache = new SoftRefVolatileCacheImp();
+	protected VolatileCache volatileCache = SoftRefVolatileCacheImp.getInstance();
 
 	protected final BlockingFetchQueues< Object > queue;
 
